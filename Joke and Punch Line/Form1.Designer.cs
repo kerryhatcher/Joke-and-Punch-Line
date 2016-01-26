d339edb3c1e2473985b3a1596f00bb48adb7369b
Kerry Hatcher <kerry.hatcher@autotrader.com>
2016-01-26 08:52:35 -0500

    inital commit

---
+++ Joke and Punch Line/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Joke_and_Punch_Line
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelText = new System.Windows.Forms.Label();
+            this.buttonSetup = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelText
+            // 
+            this.labelText.AutoSize = true;
+            this.labelText.Location = new System.Drawing.Point(12, 19);
+            this.labelText.Name = "labelText";
+            this.labelText.Size = new System.Drawing.Size(0, 13);
+            this.labelText.TabIndex = 0;
+            // 
+            // buttonSetup
+            // 
+            this.buttonSetup.Location = new System.Drawing.Point(12, 47);
+            this.buttonSetup.Name = "buttonSetup";
+            this.buttonSetup.Size = new System.Drawing.Size(98, 23);
+            this.buttonSetup.TabIndex = 1;
+            this.buttonSetup.Text = "Setup";
+            this.buttonSetup.UseVisualStyleBackColor = true;
+            this.buttonSetup.Click += new System.EventHandler(this.buttonSetup_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(140, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Punch Line";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 87);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonSetup);
+            this.Controls.Add(this.labelText);
+            this.Name = "Form1";
+            this.Text = "Joke and Punch Line";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelText;
+        private System.Windows.Forms.Button buttonSetup;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Joke and Punch Line/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Joke_and_Punch_Line
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSetup_Click(object sender, EventArgs e)
+        {
+            labelText.Text = "How do you make holy water?";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            labelText.Text = "You boil the hell out of it. ";
+        }
+    }
+}
